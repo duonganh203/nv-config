5d8ca7a6fffc07aa1ff46a44e3a42838e0372f0b
duonganh203 <rankill52@gmail.com>
2025-01-07 20:24:55 +0700

    config conform

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -7,12 +7,12 @@ local options = {
     json = { "prettier" },
     rust = { "rustfmt" },
     typescript = { "prettier" },
-    tsx = { "prettier" },
-    jsx = { "prettier" },
+    typescriptreact = { "prettier" },
+    javascriptreact = { "prettier" },
   },
 
   format_on_save = {
-  -- These options will be passed to conform.format()
+    -- These options will be passed to conform.format()
     timeout_ms = 500,
     lsp_fallback = true,
   },
